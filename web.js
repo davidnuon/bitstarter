72f5a8095feb8865f1b1e82a1f8b970ed1b18dd2
davidgaskins <david.s.gaskins@gmail.com>
2013-07-16 16:26:46 +0000

    this is trying it as the object

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var bffr = fs.readFile(index.html);
-	var str = buf.toString(bffr);
+	var str = bffr.toString();
 	response.send(str);  
 });
 
